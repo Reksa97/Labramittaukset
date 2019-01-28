ebd86864a7e9f83dce1b8aa4c802ef61ae566d32
Reko Kälkäjä <reko.kalkaja@gmail.com>
2019-01-28 20:10:35 +0200

    axios and json-server installed. measurements are now fetched from server with axios

--- app/src/App.js
+++ app/src/App.js
@@ -1,37 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 import Measurements from './components/Measurement'
-
-const measurements = [
-    {
-      id: 1,
-      name: "Hemoglobiini",
-      unit: "g/l",
-      refLow: 134,
-      refHigh: 160
-    },
-    {
-      id: 2,
-      name: "LDL-kolesteroli",
-      unit: "mmol/l",
-      refLow: 0,
-      refHigh: 3
-    },
-    {      
-      id: 3,
-      name: "Natrium (P-Na)",
-      unit: "mmol/l",
-      refLow: 137,
-      refHigh: 145
-    }
-  ]
+import measurementService from './services/measurements'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+        measurements: []
+    }
+    console.log('constructor')
+}
+
+async componentDidMount() {
+    console.log('did mount')
+
+    const response = await measurementService.getAll()
+    this.setState({ measurements: response })
+    console.log(this.state.measurements)
+}
+
   render() {
     return (
       <div className="App">
         <h1>Labramittaukset</h1>
-        <Measurements measurements={measurements} />
+        <Measurements measurements={this.state.measurements} />
       </div>
     );
   }
